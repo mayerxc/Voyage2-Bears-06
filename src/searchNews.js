d8ab0dcb25d89acf932297c6f453799c83407aaf
RL Brown <rlbrown72@gmail.com>
2017-10-24 14:12:59 -0400

    handle /news category search-term

--- src/searchNews.js
+++ src/searchNews.js
@@ -1,17 +1,55 @@
 var axios = require('axios');
 
-function searchNews(searchTermArr, key, url) {
-
+function searchNews(searchTermArr, key, url, category) {
+  var dbCat = '';
+  switch (category) {
+    case '':
+      break;
+    case 'finance':
+      dbCat = 'dmoz/Business';
+      break;
+    case 'pop':
+      dbCat = 'dmoz/Society/People/Celebrities';
+      break;
+    case 'tech':
+      dbCat = 'dmoz/Computers';
+      break;
+    case 'science':
+      dbCat = 'dmoz/Science';
+      break;
+    case 'sports':
+      dbCat = 'dmoz/Sports';
+      break;
+    case 'news':
+      dbCat = 'dmoz/Society/'; //Regional/North_America never returned anything.
+      break;
+    default:
+      break;
+  }
   var newsMessage = {
     icon_emoji: ':newspaper:',
     text: 'The latest headlines for ' + searchTermArr.join(' '),
     attachments: [],
   };
 
+  var queryURL =
+    'http://eventregistry.org/json/article?query={"$query":{"$and":[{"keyword":{"$and":' +
+    JSON.stringify(searchTermArr) +
+    '}},{"lang":"eng"}]}}&action=getArticles&resultType=articles&articlesSortBy=date&articlesCount=10&apiKey=' +
+    key;
+  if (dbCat !== '') {
+    queryURL =
+      'http://eventregistry.org/json/article?query={"$query":{"$and":[{"categoryUri":"' +
+      dbCat +
+      '"},{"keyword":{"$and":' +
+      JSON.stringify(searchTermArr) +
+      '}},{"lang":"eng"}]}}&action=getArticles&resultType=articles&articlesSortBy=date&articlesCount=10&apiKey=' +
+      key;
+  }
+  console.log(queryURL);
   axios
-    .get('http://eventregistry.org/json/article?query={"$query":{"$and":[{"keyword":{"$and":' + JSON.stringify(searchTermArr) + '}},{"lang":"eng"}]}}&action=getArticles&resultType=articles&articlesSortBy=date&articlesCount=10&apiKey=' + key)
-    .then(function (reply) {
-
+    .get(queryURL)
+    .then(function(reply) {
       // get relevant information from articles
       reply.data.articles.results.forEach(function(result) {
         if (!result.isDuplicate) {
@@ -27,11 +65,24 @@ function searchNews(searchTermArr, key, url) {
 
       // return relevant information
       axios.post(url, newsMessage).then(function() {
-        console.log('searched news for "' + searchTermArr.join(' ') + '"" at ' + new Date());
+        var source = dbCat !== '' ? dbCat : 'all categories';
+        console.log(
+          'searched news in ' +
+            source +
+            ' for "' +
+            searchTermArr.join(' ') +
+            '" at ' +
+            new Date()
+        );
       });
     })
     .catch(function(e) {
       console.log('error getting news from newsAPI: ' + e);
+      newsMessage.attachments.push({
+        fallback: 'an error occured',
+        text: 'Something went wrong with that search, on the API side.',
+      });
+      axios.post(url, newsMessage);
     });
 }
 
